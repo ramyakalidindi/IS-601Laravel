8989b450ddacccb10cf6c9a704710e27609447c3
ramya <ramya.mounami@gmail.com>
2018-10-15 23:21:51 -0400

    Add NJIT information on home page

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.def')
+@section('content')
+
+    <div class="container">
+
+    <h1>NJIT seminar halls</h1>
+
+
+
+
+        <p>Hearty welcome to the students of NJIT! The Fall semester is here.
+            At NJIT, we believe in a smarter approach to education that pairs world-class instructions in our seminar halls and labs across the globe with real-world professional experience that leads to career success.As one of the most diverse institutions in the nation, NJIT offers a place for everyone.
+            NJIT is dedicated to academic excellence for everyone. Our students benefit from world-class instruction, meaningful research and career oppurtunities from the humanities to business,the sciences and beyond.</p>
+        <p> University seminar halls are frequently used to hold meetings,trainings or conferences. University seminar halls often house state-of-the-art meeting facilities. MOst of the seminar halls at NJIThas been located right in the heart of the university. NJIT's academic environment provides a wide range of seminar settings specially designed to facilitate an optimal learning experience. Our facilities are capable of accommodating groups ranging from a few individuals to a few hundred.</p>
+
+
+
+    </div>
+
+
+@stop
+
+
+
+
+
+
+
+
+
+
